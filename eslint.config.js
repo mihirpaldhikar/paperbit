8e22fa3ab0454aa23bc961fa73bf78d7eec5c29a
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2025-01-24 22:37:40 +0530

    style: Add Prettier to format code and apply styling across the codebase.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,35 @@
+/*
+ * Copyright (c) [Mihir Paldhikar](https://mihirpaldhikar.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import globals from "globals";
 import pluginJS from "@eslint/js";
 import pluginTS from "typescript-eslint";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts}"]},
-    {languageOptions: {globals: {...globals.browser, ...globals.node}}},
-    pluginJS.configs.recommended,
-    ...pluginTS.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJS.configs.recommended,
+  ...pluginTS.configs.recommended,
+  eslintConfigPrettier,
 ];
