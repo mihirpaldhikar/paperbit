21df75cc917e965145410dcb5c8d3fbf6372b16a
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2025-01-24 22:39:07 +0530

    chore: Update License Declaration in the file header.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) [Mihir Paldhikar](https://mihirpaldhikar.com)
+ * Copyright (c) Mihir Paldhikar
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the “Software”), to deal
